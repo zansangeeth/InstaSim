1b6de832acb237429bb6fdf85b11c92d08593a72
zansangeeth <zansangeeth17@gmail.com>
2022-06-21 03:21:01 +0530

    Firebase user authentication created with null safety

--- app/src/main/java/com/zasa/instasim/LoginActivity.kt
+++ app/src/main/java/com/zasa/instasim/LoginActivity.kt
@@ -2,20 +2,45 @@ package com.zasa.instasim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
+
+private const val TAG = "LoginActivity"
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val email = etEmail.text.toString()
-        val password = etPassword.text.toString()
+        btnLogin.setOnClickListener {
 
-        if (email.isBlank() || password.isBlank()){
-            Toast.makeText(this, "email/password cannot be empty", Toast.LENGTH_SHORT).show()
-            return
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "email/password cannot be empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            //Firebase authentication checking
+            val auth = FirebaseAuth.getInstance()
+            auth.signInWithEmailAndPassword(email,password).addOnCompleteListener {
+                    task -> if (task.isSuccessful){
+                Toast.makeText(this, "Success!!", Toast.LENGTH_SHORT).show()
+                goPostActivity()
+            }else{
+                Log.e(TAG, "signInWithEmailFailed", task.exception)
+                Toast.makeText(this,"Authentication Failed", Toast.LENGTH_SHORT).show()
+            }
+
+            }
         }
+
+
+    }
+
+    private fun goPostActivity() {
+        Log.i(TAG, "goPostActivity")
     }
 }
