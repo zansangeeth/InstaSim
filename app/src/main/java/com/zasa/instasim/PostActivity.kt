bcb5ccd7dec40acd990513e8b5d8fa8d2439df0b
zansangeeth <zansangeeth17@gmail.com>
2022-06-22 03:15:47 +0530

    added firestore database also retrieving sample data in logcat

--- app/src/main/java/com/zasa/instasim/PostActivity.kt
+++ app/src/main/java/com/zasa/instasim/PostActivity.kt
@@ -3,13 +3,32 @@ package com.zasa.instasim
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
+private const val TAG = "PostActivity"
 class PostActivity : AppCompatActivity() {
+
+    private lateinit var firebaseDb : FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+
+        firebaseDb = FirebaseFirestore.getInstance()
+        val postReference = firebaseDb.collection("posts")
+        postReference.addSnapshotListener { snapshot, error ->
+            if (error != null || snapshot == null){
+                Log.i(TAG, "Exception when getting data", error)
+                return@addSnapshotListener
+            }
+            for (document in snapshot.documents){
+                Log.i(TAG, "Document ${document.id} : ${document.data}")
+            }
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
