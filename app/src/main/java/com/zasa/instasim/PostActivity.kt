88b7524f6fce218b1af9f0bf914f842debef1d7a
zansangeeth <zansangeeth17@gmail.com>
2022-06-22 03:52:49 +0530

    added other firestore functionalities- limit, orderedBy

--- app/src/main/java/com/zasa/instasim/PostActivity.kt
+++ app/src/main/java/com/zasa/instasim/PostActivity.kt
@@ -7,25 +7,30 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.zasa.instasim.models.Post
 
 private const val TAG = "PostActivity"
+
 class PostActivity : AppCompatActivity() {
 
-    private lateinit var firebaseDb : FirebaseFirestore
+    private lateinit var firebaseDb: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
 
         firebaseDb = FirebaseFirestore.getInstance()
-        val postReference = firebaseDb.collection("posts")
+        val postReference = firebaseDb
+            .collection("posts")
+            .limit(20)
+            .orderBy("creation_time_in_ms", Query.Direction.DESCENDING)
         postReference.addSnapshotListener { snapshot, error ->
-            if (error != null || snapshot == null){
+            if (error != null || snapshot == null) {
                 Log.i(TAG, "Exception when getting data", error)
                 return@addSnapshotListener
             }
             val postList = snapshot.toObjects(Post::class.java)
-            for (post in postList){
+            for (post in postList) {
                 Log.i(TAG, "Post $post")
             }
         }
@@ -38,7 +43,7 @@ class PostActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.mProfile){
+        if (item.itemId == R.id.mProfile) {
             val profileIntent = Intent(this, ProfileActivity::class.java)
             startActivity(profileIntent)
         }
